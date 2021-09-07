9217ce75cbde9cdb2675a83d9311da7ae21fc30d
LanceHefner <hefnerlance1@gmail.com>
2021-09-07 14:51:22 -0600

    starting point houses form submission broken

--- app/Controllers/HousesController.js
+++ app/Controllers/HousesController.js
@@ -1,26 +1,67 @@
+import { ProxyState } from "../AppState.js"
+import { getHouseFormTemplate } from "../forms/houseform.js"
+import { housesservice } from "../Services/HousesService.js"
+
 function _drawHouses() {
-  document.getElementById('listings').innerHTML = '<h1>TODO build the houses template</h1>'
+  let template= ''
+ 
+  ProxyState.houses.forEach(house => template+=house.HouseTemplate )
+  document.getElementById('listings').innerHTML = template
+
 }
 
 export class HousesController {
   constructor() {
     // TODO register the listener for houses
+    ProxyState.on('houses', _drawHouses)
+  }
+  async addHouse(){
+    event.preventDefault();
+    /**
+     * @type {HTMLFormElement}
+     */
+    // @ts-ignore
+    const form= event.target
+    const houseData ={
+      size: form.size.value,
+      rooms: form.bedrooms.value,
+      bathrooms: form.bathrooms.value,
+      price: form.price.value,
+      description: form.description.value,
+      imgUrl: form.imgUrl.value
+      
+      
+    }
+    try{ 
+      await housesservice.addHouse(houseData)
+    } catch (e){
+      form.make.classList.add('border-danger')
+      console.error('[TODO] you were supposed to do this', e)
+      return
+    
+    } form.reset();
+    }
+
+  showLot() {
+
+    // @ts-ignore
+    document.getElementById('lot').textContent = event.target.value
   }
 
   showHouses() {
     _drawHouses()
-    document.getElementById('controls').innerHTML = `
+    document.getElementById('controls').innerHTML = /*html*/`
       <button class="btn btn-success" onclick="app.housesController.toggleHouseForm()">Add House</button>
     `
     // TODO build the house form
     // abstract it if you want
-    document.getElementById('forms').innerHTML = /*html*/`
-      <form></form>
-    `
+    document.getElementById('forms').innerHTML = getHouseFormTemplate()
   }
 
   toggleHouseForm() {
     console.log('[TODO fill me in]')
+    document.getElementById('house-form').classList.toggle('visually-hidden')
+    
   }
 
 }
---
+++ app/Models/House.js
@@ -0,0 +1,39 @@
+import { generateId } from "../Utils/generateId.js";
+
+export class House{
+    constructor(houseData){
+        this.id = houseData.id,
+        this.size = houseData.size,
+        this.rooms = houseData.bedrooms,
+        this.bathrooms = houseData.bathrooms,
+        this.price = houseData.price,
+        // this.lot = houseData.lot
+        this.description = houseData.description
+        this.imgUrl = houseData.imgUrl
+    }
+    get HouseTemplate(){
+        return /*html*/`
+    <div class="col-lg-3 mb-4 listing">
+      <div class="card">
+        <img src="${this.imgUrl}" alt="listing image" class="rounded">
+        <div class="card-body">
+          <h5 class="d-flex justify-content-between">
+            <span > Size: ${this.size} - Rooms:${this.rooms}</span>
+            <span>$ ${this.price}</span>
+          </h5>
+          <p>${this.description}</p>
+        </div>
+      </div>
+    </div>
+    `
+  }
+    
+}
+// "_id": "60f7557e86cd7700158845db",
+//   "bedrooms": 3,
+//   "bathrooms": 2,
+//   "levels": 1,
+//   "year": 1960,
+//   "price": 3000,
+//   "imgUrl": "https://images.unsplash.com/photo-1592658221410-0f5c3615e7fc?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8b2xkJTIwaG91c2V8ZW58MHx8MHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+//   "description":
